73e32720e8b208ee0bbbd332056e72084bd48da3
Yvan Gauthier <yvan.gauthier06@icloud.com>
2025-06-30 22:33:12 +0200

    LoginView started

---
+++ Views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  ToDoList
+//
+//  Created by Yvan Gauthier on 30/06/2025.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
