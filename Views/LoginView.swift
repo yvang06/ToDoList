5b8e50de5957d061e8b045d499465fed980e310a
Yvan Gauthier <yvan.gauthier06@icloud.com>
2025-06-30 22:51:22 +0200

    Create account implimentation done.

--- Views/LoginView.swift
+++ Views/LoginView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack{
+                // Header
+                HeaderView()
+                
+                // Login Form
+                Form {
+                    TextField("Email Adress", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    TextField("Password", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    Button {
+                        // Attempt log in
+                    } label : {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color.blue)
+                            Text("Log In")
+                                .foregroundColor(Color.white)
+                                .bold()
+                        }
+                    }
+                    
+                }
+                // Create Account
+                VStack {
+                    Text("New arround here ? ")
+                    NavigationLink("Create an account", destination: RegisterView())
+                }
+                .padding(.bottom, 50)
+                
+                Spacer()
+            }
+        }
     }
 }
 
